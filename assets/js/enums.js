ea45d810cf4de012be32e41acaece54217aa6a6d
lambebr <brend.lambert@phpro.be>
2023-01-05 15:50:36 +0100

    First commit

---
+++ assets/js/enums.js
@@ -0,0 +1,159 @@
+// todo: split this file in multiple files
+
+class Helper {
+    static deepCopy = function (object) {
+        return JSON.parse(JSON.stringify(object));
+    };
+}
+
+const PieceType = {
+    EMPTY:  0,
+    PAWN:   1,
+    ROOK:   2,
+    KNIGHT: 3,
+    BISHOP: 4,
+    KING:   5,
+    QUEEN:  6
+};
+
+const Color = {
+    BLACK: 1,
+    WHITE: 2
+};
+
+const PIECE_EVALUATIONS = [];
+PIECE_EVALUATIONS[PieceType.EMPTY] = 0;
+PIECE_EVALUATIONS[PieceType.PAWN] = 1;
+PIECE_EVALUATIONS[PieceType.ROOK] = 5;
+PIECE_EVALUATIONS[PieceType.KNIGHT] = 3;
+PIECE_EVALUATIONS[PieceType.BISHOP] = 3.5;
+PIECE_EVALUATIONS[PieceType.KING] = 0;
+PIECE_EVALUATIONS[PieceType.QUEEN] = 9;
+
+class Coordinates {
+    static get INVALID() {
+        return new Coordinates();
+    }
+
+    constructor(x = -1, y = -1) {
+        this.x = x;
+        this.y = y;
+    }
+
+    equals(coordinates) {
+        return this.x === coordinates.x && this.y === coordinates.y;
+    }
+
+    isInvalid() {
+        return this.x < 0 || 7 < this.x || this.y < 0 || 7 < this.y;
+    }
+}
+
+class Move {
+    static get INVALID() {
+        return new Move();
+    }
+
+    constructor(coordinates = Coordinates.INVALID, newCoordinates = Coordinates.INVALID) {
+        this.position = coordinates;
+        this.newPosition = newCoordinates;
+    }
+
+    isInvalid() {
+        return this.position.isInvalid() || this.newPosition.isInvalid();
+    }
+}
+
+class Piece {
+    constructor(type, color = null) {
+        this.color = color;
+        this.type = type;
+
+        if (type !== PieceType.EMPTY && color === null) {
+            throw new Error('Color must be set if the piece type does not equal [PIECES.EMPTY]!');
+        }
+    }
+}
+
+class Board {
+    static get DEFAULT_BOARD_SETUP() {
+        return [
+            [new Piece(PieceType.ROOK, Color.BLACK), new Piece(PieceType.KNIGHT, Color.BLACK), new Piece(PieceType.BISHOP, Color.BLACK), new Piece(PieceType.QUEEN, Color.BLACK), new Piece(PieceType.KING, Color.BLACK), new Piece(PieceType.BISHOP, Color.BLACK), new Piece(PieceType.KNIGHT, Color.BLACK), new Piece(PieceType.ROOK, Color.BLACK)],
+            [new Piece(PieceType.PAWN, Color.BLACK), new Piece(PieceType.PAWN, Color.BLACK), new Piece(PieceType.PAWN, Color.BLACK), new Piece(PieceType.PAWN, Color.BLACK), new Piece(PieceType.PAWN, Color.BLACK), new Piece(PieceType.PAWN, Color.BLACK), new Piece(PieceType.PAWN, Color.BLACK), new Piece(PieceType.PAWN, Color.BLACK)],
+            [new Piece(PieceType.EMPTY), new Piece(PieceType.EMPTY), new Piece(PieceType.EMPTY), new Piece(PieceType.EMPTY), new Piece(PieceType.EMPTY), new Piece(PieceType.EMPTY), new Piece(PieceType.EMPTY), new Piece(PieceType.EMPTY)],
+            [new Piece(PieceType.EMPTY), new Piece(PieceType.EMPTY), new Piece(PieceType.EMPTY), new Piece(PieceType.EMPTY), new Piece(PieceType.EMPTY), new Piece(PieceType.EMPTY), new Piece(PieceType.EMPTY), new Piece(PieceType.EMPTY)],
+            [new Piece(PieceType.EMPTY), new Piece(PieceType.EMPTY), new Piece(PieceType.EMPTY), new Piece(PieceType.EMPTY), new Piece(PieceType.EMPTY), new Piece(PieceType.EMPTY), new Piece(PieceType.EMPTY), new Piece(PieceType.EMPTY)],
+            [new Piece(PieceType.EMPTY), new Piece(PieceType.EMPTY), new Piece(PieceType.EMPTY), new Piece(PieceType.EMPTY), new Piece(PieceType.EMPTY), new Piece(PieceType.EMPTY), new Piece(PieceType.EMPTY), new Piece(PieceType.EMPTY)],
+            [new Piece(PieceType.PAWN, Color.WHITE), new Piece(PieceType.PAWN, Color.WHITE), new Piece(PieceType.PAWN, Color.WHITE), new Piece(PieceType.PAWN, Color.WHITE), new Piece(PieceType.PAWN, Color.WHITE), new Piece(PieceType.PAWN, Color.WHITE), new Piece(PieceType.PAWN, Color.WHITE), new Piece(PieceType.PAWN, Color.WHITE)],
+            [new Piece(PieceType.ROOK, Color.WHITE), new Piece(PieceType.KNIGHT, Color.WHITE), new Piece(PieceType.BISHOP, Color.WHITE), new Piece(PieceType.QUEEN, Color.WHITE), new Piece(PieceType.KING, Color.WHITE), new Piece(PieceType.BISHOP, Color.WHITE), new Piece(PieceType.KNIGHT, Color.WHITE), new Piece(PieceType.ROOK, Color.WHITE)],
+        ];
+    }
+
+    constructor(id, setup = null, turn = Color.WHITE, previousMove = new Move()) {
+        this.id = id;
+        this.setup = setup;
+        this.turn = turn;
+        this.previousMove = previousMove;
+    }
+
+    createDefaultSetup() {
+        this.setup = Helper.deepCopy(Board.DEFAULT_BOARD_SETUP);
+        return this;
+    }
+
+    getPiece(coordinates) {
+        return this.setup[coordinates.x][coordinates.y];
+    }
+
+    findKingCoordinates(color) {
+        this.setup.forEach((column, x) => {
+            column.forEach((piece, y) => {
+                if (piece.type === PieceType.KING && piece.color === color) {
+                    return new Coordinates(x, y);
+                }
+            });
+        });
+
+        return Coordinates.INVALID;
+    }
+
+    move(move) {
+        this.setup[move.newPosition.x][move.newPosition.y] = this.setup[move.position.x][move.position.y];
+        this.setup[move.position.x][move.position.y] = new Piece(PieceType.EMPTY);
+
+        return this;
+    }
+
+    doMoveIfLegal(move) {
+        const isLegal = Rules.isLegalMove(this, move);
+
+        if(isLegal) {
+            this.move(move);
+            this.turn = this.turn === Color.WHITE ? Color.BLACK : Color.WHITE;
+            this.previousMove = move;
+
+            return true;
+        }
+
+        console.warn('Illegal move...');
+
+        return false;
+    }
+}
+
+const BoardFactory = (function () {
+    let nextId = 0;
+
+    function createInstance(setup = null) {
+        return new Board(nextId++, setup);
+    }
+
+    function createDefaultInstance() {
+        return createInstance().createDefaultSetup();
+    }
+
+    return {
+        createInstance: createInstance,
+        createDefaultInstance: createDefaultInstance
+    }
+})();
---
+++ assets/js/rules.js
@@ -0,0 +1,169 @@
+class Rules {
+    static isLegalMove(board, move) {
+        return Rules.isLegalMoveByPieceType(board, move)
+            && !Rules.doesMoveResultInCheck(board, move);
+    }
+
+    static getLegalMoves(board, coordinates) {
+        const POSSIBLE_MOVES = Rules.getPossibleMovesByPieceType(board, coordinates);
+        const LEGAL_MOVES = [];
+
+        POSSIBLE_MOVES.forEach(possibleMove => {
+            if (Rules.isLegalMove(board, possibleMove)) {
+                LEGAL_MOVES.push(possibleMove);
+            }
+        });
+
+        return LEGAL_MOVES;
+    }
+
+    static getPossibleMovesByPieceType(board, coordinates) {
+        switch (board.getPiece(coordinates).type) {
+            case PieceType.EMPTY:
+                return [];
+            case PieceType.ROOK:
+                return Rules.getPossibleRookMoves(coordinates);
+            case PieceType.KNIGHT:
+                return Rules.getPossibleKnightMoves(coordinates);
+            default:
+                return [];
+        }
+    }
+
+    static isLegalMoveByPieceType(board, move) {
+        if (
+            move.isInvalid()
+            || !Rules.isFriendlyPiece(board, move.position)
+            || Rules.isMoveToFriendlyField(board, move)
+        ) {
+            return false;
+        }
+
+        switch (board.getPiece(move.position).type) {
+            case PieceType.EMPTY:
+                return false;
+            case PieceType.ROOK:
+                return Rules.isLegalRookMove(board, move);
+            case PieceType.KNIGHT:
+                return Rules.isLegalKnightMove(board, move);
+            case PieceType.BISHOP:
+                return Rules.isLegalBishopMove(board, move);
+            default:
+                return true;
+        }
+    }
+
+    static isFriendlyPiece(board, coordinates) {
+        return board.getPiece(coordinates).color === board.turn;
+    }
+
+    static isMoveToFriendlyField(board, move) {
+        const PIECE = board.getPiece(move.position);
+        const NEW_PIECE = board.getPiece(move.newPosition);
+
+        return NEW_PIECE.type !== PieceType.EMPTY && PIECE.color === NEW_PIECE.color;
+    }
+
+    static doesMoveResultInCheck(board, move) {
+        const NEW_BOARD = BoardFactory.createInstance(Helper.deepCopy(board.setup));
+        NEW_BOARD.move(move);
+
+        return Rules.isCheck(NEW_BOARD);
+    }
+
+    static isCheck(board) {
+        const KING_COORDINATES = board.findKingCoordinates(board.turn);
+
+        if (KING_COORDINATES.isInvalid()) {
+            return false;
+        }
+
+        // todo: find all opposite color pieces and see if capturing the king is legal
+
+        return false;
+    }
+
+    static getPossibleRookMoves(coordinates) {
+        let res = [];
+
+        for (let i = 0; i < 8; i++) {
+            res.push(new Move(coordinates, new Coordinates(coordinates.x, i)));
+            res.push(new Move(coordinates, new Coordinates(i, coordinates.y)));
+        }
+
+        return res;
+    }
+
+    static isLegalRookMove(board, move) {
+        let smallIndex, bigIndex, searchArray, isYConstant = false;
+
+        if (move.position.x === move.newPosition.x) {
+            searchArray = board.setup[move.position.x];
+
+            if (move.position.y < move.newPosition.y) {
+                smallIndex = move.position.y;
+                bigIndex = move.newPosition.y;
+            } else if (move.newPosition.y < move.position.y) {
+                smallIndex = move.newPosition.y;
+                bigIndex = move.position.y;
+            } else {
+                return false;
+            }
+        } else if (move.position.y === move.newPosition.y) {
+            searchArray = board.setup;
+            isYConstant = true;
+
+            if (move.position.x < move.newPosition.x) {
+                smallIndex = move.position.x;
+                bigIndex = move.newPosition.x;
+            } else if (move.newPosition.x < move.position.x) {
+                smallIndex = move.newPosition.x;
+                bigIndex = move.position.x;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+        for (let i = smallIndex + 1; i < bigIndex; i++) {
+            const PIECE = isYConstant ? searchArray[i][move.position.y] : searchArray[i];
+
+            if (PIECE.type !== PieceType.EMPTY) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static getPossibleKnightMoves(coordinates) {
+        return [
+            new Move(coordinates, new Coordinates(coordinates.x - 2, coordinates.y - 1)),  // 2 up,    1 left
+            new Move(coordinates, new Coordinates(coordinates.x - 2, coordinates.y + 1)),  // 2 up,    1 right
+            new Move(coordinates, new Coordinates(coordinates.x - 1, coordinates.y + 2)),  // 2 right, 1 up
+            new Move(coordinates, new Coordinates(coordinates.x + 1, coordinates.y + 2)),  // 2 right, 1 down
+            new Move(coordinates, new Coordinates(coordinates.x + 2, coordinates.y + 1)),  // 2 down,  1 right
+            new Move(coordinates, new Coordinates(coordinates.x + 2, coordinates.y - 1)),  // 2 down,  1 left
+            new Move(coordinates, new Coordinates(coordinates.x + 1, coordinates.y - 2)),  // 2 left,  1 down
+            new Move(coordinates, new Coordinates(coordinates.x - 1, coordinates.y - 2))   // 2 left,  1 up
+        ];
+    }
+
+    static isLegalKnightMove(board, move) {
+        const POSSIBLE_MOVES = Rules.getPossibleKnightMoves(move.position);
+        let isLegal = false;
+
+        POSSIBLE_MOVES.forEach(POSSIBLE_MOVE => {
+            if (POSSIBLE_MOVE.newPosition.equals(move.newPosition)) {
+                isLegal = true;
+            }
+        });
+
+        return isLegal;
+    }
+
+    static isLegalBishopMove(board, move) {
+        // todo
+    }
+}
